ba5cf6ceb2e082ee59d5d490e9f2759bb486b9de
Kristina_Brelova <krisbrelova838@gmail.com>
2025-03-06 11:13:05 +0500

    Добавление авторизации через куки

--- Program.cs
+++ Program.cs
@@ -2,6 +2,7 @@ using Microsoft.AspNetCore.Authentication.Cookies;
 using System.Security.Claims;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using System;
 
 var adminRole = new Role("admin");
 var creatorRole  = new Role("creator");
@@ -12,16 +13,99 @@ List<Product> repo =
     new(0,"Кукла","Криссюк",0,"Вязание","Обычная кукла",100),
 ];
 
-List<User> repousers =
-[
-    new(0,"Krissyuk","Krissyuk","123","женский",adminRole),
-];
+var repousers = new List<User>
+{
+    new ("tom@gmail.com", "12345", adminRole),
+    new ("bob@gmail.com", "55555", userRole),
+};
 
 var builder = WebApplication.CreateBuilder();
 builder.Services.AddCors();
+builder.Services.AddAuthentication(CookieAuthenticationDefaults.AuthenticationScheme)
+    .AddCookie(options =>
+    {
+        options.LoginPath = "/login";
+        options.AccessDeniedPath = "/accessdenied";
+    });
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
+app.UseAuthentication();
+app.UseAuthorization();
+
 app.UseCors(a => a.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+
+app.MapGet("/accessdenied", async (HttpContext context) =>
+{
+    context.Response.StatusCode = 403;
+    await context.Response.WriteAsync("Access Denied");
+});
+app.MapGet("/login", async (HttpContext context) =>
+{
+    context.Response.ContentType = "text/html; charset=utf-8";
+    string loginForm = @"<!DOCTYPE html>
+    <html>
+    <head>
+        <meta charset='utf-8' />
+        <title>METANIT.COM</title>
+    </head>
+    <body>
+        <h2>Login Form</h2>
+        <form method='post'>
+            <p>
+                <label>Email</label><br />
+                <input name='email' />
+            </p>
+            <p>
+                <label>Password</label><br />
+                <input type='password' name='password' />
+            </p>
+            <input type='submit' value='Login' />
+        </form>
+    </body>
+    </html>";
+    await context.Response.WriteAsync(loginForm);
+});
+
+app.MapPost("/login", async (string? returnUrl, HttpContext context) =>
+{
+    // получаем из формы email и пароль
+    var form = context.Request.Form;
+    // если email и/или пароль не установлены, посылаем статусный код ошибки 400
+    if (!form.ContainsKey("email") || !form.ContainsKey("password"))
+        return Results.BadRequest("Email и/или пароль не установлены");
+    string email = form["email"];
+    string password = form["password"];
+
+    // находим пользователя 
+    User? user = repousers.FirstOrDefault(p => p.Email == email && p.Password == password);
+    // если пользователь не найден, отправляем статусный код 401
+    if (user is null) return Results.Unauthorized();
+    var claims = new List<Claim>
+    {
+        new Claim(ClaimsIdentity.DefaultNameClaimType, user.Email),
+        new Claim(ClaimsIdentity.DefaultRoleClaimType, user.Role.Name)
+    };
+    var claimsIdentity = new ClaimsIdentity(claims, "Cookies");
+    var claimsPrincipal = new ClaimsPrincipal(claimsIdentity);
+    await context.SignInAsync(claimsPrincipal);
+    return Results.Redirect(returnUrl ?? "/");
+});
+// доступ только для роли admin
+app.Map("/admin", [Authorize(Roles = "admin")] () => "Admin Panel");
+
+// доступ только для ролей admin и user
+app.Map("/", [Authorize(Roles = "admin, user")] (HttpContext context) =>
+{
+    var login = context.User.FindFirst(ClaimsIdentity.DefaultNameClaimType);
+    var role = context.User.FindFirst(ClaimsIdentity.DefaultRoleClaimType);
+    return $"Name: {login?.Value}\nRole: {role?.Value}";
+});
+app.MapGet("/logout", async (HttpContext context) =>
+{
+    await context.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
+    return "Данные удалены";
+});
 
 string message = "";
 
@@ -77,14 +161,17 @@ class Product(int id, string name, string creator, int creatorId, string categor
     public double Price { get; set; } = price;
 }
 
-class User(int id, string nickName, string login, string password, string gender, Role role)
+class User
 {
-    public int Id { get; set; } = id;
-    public string NickName { get; set; } =nickName;
-    public string Login { get; set; } = login;
-    public string Password { get; set; }=password;
-    public string Gender { get; set; }=gender;
-    public Role Role { get; set; }=role;
+    public string Email { get; set; }
+    public string Password { get; set; }
+    public Role Role { get; set; }
+    public User(string email, string password, Role role)
+    {
+        Email = email;
+        Password = password;
+        Role = role;
+    }
 }
 class Role
 {
